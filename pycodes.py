2ae5595915036e78868a845c8e9da72785088650
Thomas Robacker <trobacker21@gmail.com>
2022-03-04 07:56:42 -0500

    Create pycodes.py

---
+++ pycodes.py
@@ -0,0 +1,5 @@
+## A doc of useful Python code/functions
+
+# Number of missing values in each column of training data
+missing_val_count_by_column = (X_train.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
